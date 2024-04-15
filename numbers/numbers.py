482b08c3c07fbaa6f060838b4de321bfe957e388
Nikita Khomenko <mishanikita261202@mail.ru>
2024-04-15 13:10:28 +0300

    Create numbers.py

---
+++ numbers/numbers.py
@@ -0,0 +1,26 @@
+number0 = 555
+print(number0)
+
+number00 = 100_000_000_000_000_000 # нижнее подчеркивание испаользуется для визуального разделения на разряды
+print(number00)
+
+number1 = 5 + 8 # сложение
+print(number1)
+
+number2 = 8 - 3 # вычитание
+print(number2)
+
+number3 = 8 * 6 # умножение
+print(number3)
+
+number4 = 8 / 2 # деление
+print(number4)
+
+number5 = 8 % 3 # остаток от деления
+print(number5)
+
+number6 = 8 // 3 # целая часть от деления
+print(number6)
+
+number7 = 8 ** 3 # возведение в степень
+print(number7)
