1dfb20795d5bc95ee3022b8b66e054141f7b0e0d
Nikita Khomenko <mishanikita261202@mail.ru>
2024-04-15 13:10:35 +0300

    Create favorite_language.py

---
+++ strings/favorite_language.py
@@ -0,0 +1,10 @@
+favorite_language = ' python '
+
+favorite_language.rstrip() # убирает отступы справа
+print(favorite_language)
+
+favorite_language.lstrip() # убирает отступы слева
+print(favorite_language)
+
+favorite_language.strip() # убирает отступы по краям (и слева и справа)
+print(favorite_language)
