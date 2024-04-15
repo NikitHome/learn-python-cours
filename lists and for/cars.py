fb9f984b4e1cf215747043788fb01b20c4ade793
Nikita Khomenko <mishanikita261202@mail.ru>
2024-04-15 13:10:18 +0300

    Create cars.py

---
+++ lists and for/cars.py
@@ -0,0 +1,15 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse() # вывод списка в обратном порядке
+print(cars)
+
+cars.sort() # сортировка по алфавиту
+print(cars)
+
+cars.sort(reverse=True) # сортировка по алфавиту в обратном порядке
+print(cars)
+
+print(sorted(cars)) # временная сортировка списка
+
+print(len(cars)) # len - длина списка
