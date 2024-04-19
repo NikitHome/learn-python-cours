22ec39e0544e87aaee602eb2e307d0a64716d958
Nikita Khomenko <mishanikita261202@mail.ru>
2024-04-19 12:19:24 +0300

    Update apostrophe.py

--- strings/apostrophe.py
+++ strings/apostrophe.py
@@ -1,2 +1,10 @@
 message = "One of Python's strengths is its diverse community."
 print(message)
+
+# Апостроф находится в строке, заключенной в двойные кавычки, так что у 
+# интерпретатора Python не возникает проблем с правильной интерпретацией 
+# следующей строки:
+# One of Python's strengths is its diverse community.
+
+# Однако при использовании одиночных ковычек Python не сможет определить где 
+# должна заканчиваться строка.
