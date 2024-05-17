2766357f07a8f3fc4ef7bf93c69c6129ced8e561
Nikita Khomenko <mishanikita261202@mail.ru>
2024-05-17 12:03:15 +0300

    Create alien.py

---
+++ dictionary/alien.py
@@ -0,0 +1,18 @@
+alien_0 = {}
+print(alien_0)
+
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0)
+print(alien_0['color'])
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+alien_0['x_position'] = 5
+print(alien_0)
