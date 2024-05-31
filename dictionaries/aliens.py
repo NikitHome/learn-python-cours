69a1d54728dd74d3725a9a5fcc88d0538256b8b3
Nikita Khomenko <mishanikita261202@mail.ru>
2024-05-31 17:22:47 +0300

    Update aliens.py

--- dictionaries/aliens.py
+++ dictionaries/aliens.py
@@ -21,3 +21,24 @@ aliens = [alien_0, alien_1, alien_2]
 for alien in aliens:
     for key, value in alien.items():
         print(f"{key}: {value}")
+        
+        
+        
+aliens = []
+
+for alien_number in range(30):
+    
+    new_alien = {
+        'color': 'green',
+        'points': 5,
+        'speed': 'slow',
+    }
+    
+    aliens.append(new_alien)
+    
+for alien in aliens[:5]:
+    print(alien)
+
+print("...")
+
+print(f"Total number of aliens: {len(aliens)}")
