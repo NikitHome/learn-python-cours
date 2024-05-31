cb077099e7d17d5d2a61a60005c6f75a0ef73079
Nikita Khomenko <mishanikita261202@mail.ru>
2024-05-31 17:22:50 +0300

    Update formatted_name.py

--- functions/formatted_name.py
+++ functions/formatted_name.py
@@ -11,3 +11,18 @@ print(musician)
 
 musician = get_formatted_name('jhon', 'hooker')
 print(musician)
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+            
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}")
