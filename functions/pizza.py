3fdaba68ffe6cbd0f6e64fac8ed7761a2b048db4
Nikita Khomenko <mishanikita261202@mail.ru>
2024-06-21 22:22:18 +0300

    Delete pizza.py

--- functions/pizza.py
+++
@@ -1,7 +0,0 @@
-def make_pizza(*toppings):
-    print(f"\nMaking a pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")
-    
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
