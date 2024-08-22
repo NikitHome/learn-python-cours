b76857a3c03e28ae7a93070703449a37d7776ad4
Nikita Khomenko <mishanikita261202@mail.ru>
2024-08-22 13:44:43 +0300

    Update pets.py

--- function/pets.py
+++ function/pets.py
@@ -2,5 +2,5 @@ def describe_pet(animal_type, animal_name):
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {animal_name.title()}.")
     
-describe_pet(animal_type='hamster', animal_name='harry')
+describe_pet('hamster', 'harry')
 describe_pet(animal_type='dog', animal_name='willie')
