0861639376afb2941be275493c7624c2867d17e4
Nikita Khomenko <mishanikita261202@mail.ru>
2024-09-15 16:11:16 +0300

    Delete my_cars.py

--- class/car/my_cars.py
+++
@@ -1,7 +0,0 @@
-import electric_car
-import car
-
-my_beetle = car.Car('volkswagen', 'beetle', 2019)
-print(my_beetle.get_descriptive_name())
-
-my_tesla = electric_car.ElectricCar('tesla', 'roadster', 2019)
