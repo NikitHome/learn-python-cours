431cd78d279e6d17b9cf8a3a8dc32f61a36a1dd3
Nikita Khomenko <mishanikita261202@mail.ru>
2024-09-15 16:11:34 +0300

    Delete pizza.py

--- dictionaries/pizza.py
+++
@@ -1,9 +0,0 @@
-pizza = {
-    'crust': 'thick',
-    'toppings': ['mushrooms', 'extra cheese']
-}
-
-print(f"You ordered a {pizza['crust']} - crust pizza.")
-
-for topping in pizza['toppings']:
-    print("\t" + topping)
