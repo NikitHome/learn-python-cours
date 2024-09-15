f9b9fdc6dc3e7abd3a3b9672723e2ab5c6a6e662
Nikita Khomenko <mishanikita261202@mail.ru>
2024-09-15 16:12:14 +0300

    Delete greet_users.py

--- function/greet_users.py
+++
@@ -1,7 +0,0 @@
-def greet_users(names):
-    for name in names:
-        msg = f"Hello, {name.title()}!"
-        print(msg)
-    
-usernames = ['hannah', 'ty', 'margot']
-greet_users(names=usernames)
