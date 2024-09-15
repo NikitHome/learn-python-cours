84941a4c48948c7de214e64dbf58219e7d8cca3f
Nikita Khomenko <mishanikita261202@mail.ru>
2024-09-15 16:12:11 +0300

    Delete squares.py

--- for/squares.py
+++
@@ -1,8 +0,0 @@
-squares = []
-for value in range(1, 11):
-    square = value**2 # ** - степень
-    squares.append(square)
-print(squares)
-
-squares = [value**2 for value in range(1, 11)] # генератор списка
-print(squares)
