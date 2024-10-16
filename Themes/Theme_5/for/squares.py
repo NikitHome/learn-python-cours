7c1a45c13757893eeb84c6fc3a65fa798a1cbbfb
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-16 12:02:45 +0300

    Update squares.py

--- Themes/Theme_5/for/squares.py
+++ Themes/Theme_5/for/squares.py
@@ -1,8 +1,8 @@
 squares = []
 for value in range(1, 11):
-    square = value**2 # ** - степень
+    square = value**2
     squares.append(square)
 print(squares)
 
-squares = [value**2 for value in range(1, 11)] # генератор списка
+squares = [value**2 for value in range(1, 11)]
 print(squares)
