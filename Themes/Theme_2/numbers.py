99ad39409684ef63c5a9cbb4e96801ee363f2b36
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-16 12:02:20 +0300

    Create numbers.py

---
+++ Themes/Theme_2/numbers.py
@@ -0,0 +1,43 @@
+number_0 = 555
+print(number_0)
+
+number_00 = 100_000_000_000 
+print(number_00)
+
+number_1 = 5 + 8
+print(number_1)
+
+number_2 = 8 - 3
+print(number_2)
+
+number_3 = 8 * 6
+print(number_3)
+
+number_4 = 8 / 2
+print(number_4)
+
+number_5 = 8 % 3
+print(number_5)
+
+number_6 = 8 // 3
+print(number_6)
+
+number_7 = 8 ** 3
+print(number_7)
+
+number_8 = 2 + 3 * 4
+print(number_8)
+
+number_9 = (2 + 3) * 4
+print(number_9)
+
+number_10 = 0.1 + 0.1
+print(number_10)
+
+number_11 = 2 * 0.4
+print(number_11)
+
+number_12 = 4 / 2
+print(number_12)
+
+x, y, x = 0, 0, 0
