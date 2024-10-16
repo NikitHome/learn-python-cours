1e7629f341b9efac29ffcf42c2b9a2ebd9fce348
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-16 12:02:37 +0300

    Create dimensions.py

---
+++ Themes/tuples/dimensions.py
@@ -0,0 +1,12 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1], '\n')
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+    
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
