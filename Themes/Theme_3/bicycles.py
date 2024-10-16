dea9ceedf571e00b27e04710e72ef6302d7bfec3
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-16 12:02:30 +0300

    Create bicycles.py

---
+++ Themes/Theme_3/bicycles.py
@@ -0,0 +1,13 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0]) 
+
+print(bicycles[0].title())
+
+print(bicycles[1])
+
+print(bicycles[3])
+
+print(bicycles[-1])
+
+message = f'My first bicycle was a {bicycles[0].title()}.'
+print(message)
