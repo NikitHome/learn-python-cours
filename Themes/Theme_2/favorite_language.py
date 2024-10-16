c63a2a5a16847687f6c39908e143fe169afcb014
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-16 12:02:25 +0300

    Create favorite_language.py

---
+++ Themes/Theme_2/favorite_language.py
@@ -0,0 +1,10 @@
+favorite_language = ' python '
+
+favorite_language.rstrip()
+print(favorite_language)
+
+favorite_language.lstrip()
+print(favorite_language)
+
+favorite_language.strip()
+print(favorite_language)
