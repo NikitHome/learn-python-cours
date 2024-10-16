3d00b37c028ed27174e20130c99379aac1c60c93
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-16 12:02:32 +0300

    Create cars.py

---
+++ Themes/Theme_3/cars.py
@@ -0,0 +1,15 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse() 
+print(cars)
+
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True) 
+print(cars)
+
+print(sorted(cars)) 
+
+print(len(cars)) 
