20725c34dbe663ab9c5d2d2f4cffb24498527fc1
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-16 12:02:52 +0300

    Update counting.py

--- Themes/Theme_9/while/counting.py
+++ Themes/Theme_9/while/counting.py
@@ -3,7 +3,7 @@ current_number = 0
 while current_number < 10:
     current_number += 1
     
-    if current_number % 2 == 0:
+    if current_number % 2 != 0:
         continue
     
     print(current_number)
