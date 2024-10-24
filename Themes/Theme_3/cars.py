d737a8026915a81841b14fa686620603d7b618d5
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-24 16:46:18 +0300

    Update cars.py

--- Themes/Theme_3/cars.py
+++ Themes/Theme_3/cars.py
@@ -1,15 +1,36 @@
+# сортировка
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-print(cars)
-
-cars.reverse() 
-print(cars)
-
 cars.sort()
 print(cars)
 
-cars.sort(reverse=True) 
+
+# сортировка в обратном порядке
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
 print(cars)
 
-print(sorted(cars)) 
 
-print(len(cars)) 
+# временная сортировка
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+
+# вывод списка в обратном порядке
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+
+# определение длины списка
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
