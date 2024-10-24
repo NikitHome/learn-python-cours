4864728640a364ec706b6329cb87c778413ed66b
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-24 16:46:20 +0300

    Update motorcycles.py

--- Themes/Theme_3/motorcycles.py
+++ Themes/Theme_3/motorcycles.py
@@ -1,4 +1,53 @@
+# изменение элементов в списке
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+
+# присоединение элементов в конец списка
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+
+# вставка элементов в список
 motorcycles = ['honda', 'yamaha', 'suzuki']
 
-popped_motorcycle = motorcycles.pop(0)
-print(f'The last motorcycle i owned was a {popped_motorcycle.title()}.')    
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+
+# удаление элемента с использованием команды del
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+
+
+# удаление элемента с использованием метода pop()
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+
+# извлечение элементов из произвольной позиции списка
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}")
+
+
+# удаление элементов по значению 
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
