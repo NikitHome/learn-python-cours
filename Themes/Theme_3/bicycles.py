449d9151c5a9f2ca06ad6b4551aef7060011b92e
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-24 16:46:16 +0300

    Update bicycles.py

--- Themes/Theme_3/bicycles.py
+++ Themes/Theme_3/bicycles.py
@@ -1,13 +1,29 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles) 
+
+
+# обращение к элементам списка 
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles[0]) 
 
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles[0].title())
 
-print(bicycles[1])
 
+# индексы начинаются с 0, а не с 1
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1])
 print(bicycles[3])
 
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles[-1])
 
+
+# использование отдельных элементов списка
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
 message = f'My first bicycle was a {bicycles[0].title()}.'
+
 print(message)
