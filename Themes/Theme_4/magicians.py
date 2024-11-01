f22cb9027fe5672ddcd59785a6578cb89a09697a
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-01 11:00:05 +0300

    Create magicians.py

---
+++ Themes/Theme_4/magicians.py
@@ -0,0 +1,20 @@
+# Перебор списка
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+
+# Более сложные действия в цикле for
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+        
+    
+# Выполнение действий после цикла for
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+    
+print("Thank you, everyone. That was a great magic show!\n")
