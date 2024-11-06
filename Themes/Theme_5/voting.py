a7d67c27f64f63ec38730fefc63a8c0efaf7b749
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-06 10:59:08 +0300

    Create voting.py

---
+++ Themes/Theme_5/voting.py
@@ -0,0 +1,13 @@
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+    
+    
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please registered to vote as soon you turn 18!")
