bdcc9a471dad6a0f8e78f4a9be251b5d787ff2d9
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-06 10:59:00 +0300

    Create amusement_park.py

---
+++ Themes/Theme_5/amusement_park.py
@@ -0,0 +1,42 @@
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40")
+    
+    
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 0
+    
+print(f"\nYour admission cost is ${price}.")
+
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+    
+print(f"\nYour admission cost is ${price}.")
+
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+elif age >= 65:
+    price = 20
+    
+print(f"\nYour admission cost is ${price}.")
