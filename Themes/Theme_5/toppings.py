8e30a35146c6caf67ebc3a2e5617c171ea358251
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-06 10:59:06 +0300

    Create toppings.py

---
+++ Themes/Theme_5/toppings.py
@@ -0,0 +1,60 @@
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+    
+    
+requested_topping = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_topping:
+    print('Adding mushrooms.')
+if 'pepperoni' in requested_topping:
+    print('Adding pepperoni.')
+if 'extra cheese' in requested_topping:
+    print('Adding extra cheese.')
+    
+print('\nFinishing making your pizza!')
+
+
+requested_topping = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_topping:
+    print('Adding mushrooms.')
+elif 'pepperoni' in requested_topping:
+    print('Adding pepperoni.')
+elif 'extra cheese' in requested_topping:
+    print('Adding extra cheese.')
+    
+print('\nFinishing making your pizza!')
+
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print(f'Adding {requested_topping}')
+    
+print('\nFinished making your pizza!')
+
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print('Sorry, we are out of green peppers right now.')
+    else:
+        print(f'Adding {requested_topping}')
+    
+print('\nFinished making your pizza!')
+
+
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 
+                      'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f'Adding {requested_topping}.')
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+        
+print('\nFinished making your pizza!')
