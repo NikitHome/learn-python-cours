80edce4247fcba44ee916632b8b06f5b2ea199e8
Nikita Khomenko <mishanikita261202@mail.ru>
2024-11-08 12:52:22 +0300

    Update pets.py

--- Themes/Theme_10/function/pets.py
+++ Themes/Theme_10/function/pets.py
@@ -3,4 +3,4 @@ def describe_pet(animal_type, animal_name):
     print(f"My {animal_type}'s name is {animal_name.title()}.")
     
 describe_pet('hamster', 'harry')
-describe_pet(animal_type='dog', animal_name='willie')
+describe_pet(animal_name='willie', animal_type='dog')
