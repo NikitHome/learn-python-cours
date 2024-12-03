a67feea9e3c2932278fd006fad6a9402b8bb0281
Nikita Khomenko <mishanikita261202@mail.ru>
2024-12-03 23:27:36 +0300

    Deleted

--- Themes/Theme_7/dictionaries/alien_no_points.py
+++
@@ -1,4 +0,0 @@
-alien_0 = {'color': 'green', 'speed': 'slow'}
-
-points_value = alien_0.get('points', 'No point value assigned.')
-print(points_value)
--- Themes/Theme_8/input_data/greeter.py
+++
@@ -1,6 +0,0 @@
-prompt = "If you tell us who you are, we can personalize the message you see."
-prompt += "\nWhat is your first name? " 
-
-name = input(prompt)
-
-print(f"\nHello, {name}!")
