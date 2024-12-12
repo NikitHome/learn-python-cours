b1b04c9babd04952bf1fd538bc67444ac2dc41e6
Nikita Khomenko <mishanikita261202@mail.ru>
2024-12-12 17:00:10 +0300

    Deleted

--- Themes/Theme_10/files/write_message.py
+++
@@ -1,5 +0,0 @@
-filename = 'Themes/Theme_12/files/programming.txt'
-
-with open(filename, 'w') as file_object:
-    file_object.write("I love programming!\n")
-    file_object.write("I love creating new games.\n")
