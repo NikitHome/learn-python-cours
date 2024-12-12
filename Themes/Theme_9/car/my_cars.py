3483e7f5d667a297e69834211a69764fef4dc02a
Nikita Khomenko <mishanikita261202@mail.ru>
2024-12-12 17:01:42 +0300

    Create my_cars.py

---
+++ Themes/Theme_9/car/my_cars.py
@@ -0,0 +1,7 @@
+import electric_car
+import car
+
+my_beetle = car.Car('volkswagen', 'beetle', 2019)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = electric_car.ElectricCar('tesla', 'roadster', 2019)
