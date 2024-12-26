fd137e8e245230f5ba08e29fa589d73b4cb3ebc1
Nikita Khomenko <mishanikita261202@mail.ru>
2024-12-26 14:24:01 +0300

    Update mountain_poll.py

--- Themes/Theme_7/mountain_poll.py
+++ Themes/Theme_7/mountain_poll.py
@@ -1,17 +1,22 @@
 responses = {}
 
+# Устанвока флага продолжения опроса.
 polling_active = True
 
 while polling_active:
+    # Запрос имени и ответа пользователя.
     name = input("\nWhat is your name? ")
     response = input("Which mountain would you like to climb someday? ")
     
+    # Ответ сохраняется в словаре:
     responses[name] = response
     
+    # Проверка продолжения опроса.
     repeat = input("Would you like to let another person respond? (yes/no) ")
     if repeat == 'no':
         polling_active = False
         
+# Опрос завершен, вывести результаты.
 print("\n--- Poll Results ---")
 for name, response in responses.items():
     print(f"{name.title()} would like to climb {response}.")
