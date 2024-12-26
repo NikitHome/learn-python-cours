cf0c520bad7ca1a77bd76f9ed7edeccba88d8aa0
Nikita Khomenko <mishanikita261202@mail.ru>
2024-12-26 14:23:56 +0300

    Update confirmed_users.py

--- Themes/Theme_7/confirmed_users.py
+++ Themes/Theme_7/confirmed_users.py
@@ -1,12 +1,18 @@
+# Начинаем с двух списков: пользователей для проверки 
+# и пустого списка для хранения проверенных пользователей.
 unconfirmed_users = ['alice', 'brian', 'candace']
 confirmed_users = []
 
+# Проверяем каждого пользователя, пока остаются непроверенные
+# пользователи. Каждый пользователь, прошедший проверку,
+# перемещавется в список проверенных.
 while unconfirmed_users:
     current_user = unconfirmed_users.pop()
     
     print(f"Verifying user: {current_user.title()}")
     confirmed_users.append(current_user)
     
+# Вывод всех проверенных пользователей.
 print("\nThe following users have been confirmed:")
 for confirmed_user in confirmed_users:
     print(confirmed_user.title())
