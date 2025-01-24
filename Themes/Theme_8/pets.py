778a6faedcf727d35c47f7436ff0beac16d4ac47
Nikita Khomenko <mishanikita261202@mail.ru>
2025-01-24 13:51:17 +0300

    Update pets.py

--- Themes/Theme_8/pets.py
+++ Themes/Theme_8/pets.py
@@ -1,6 +1,38 @@
-def describe_pet(animal_type, animal_name):
+def describe_pet(animal_type, pet_name):
     print(f"\nI have a {animal_type}.")
-    print(f"My {animal_type}'s name is {animal_name.title()}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
     
 describe_pet('hamster', 'harry')
-describe_pet(animal_name='willie', animal_type='dog')
+describe_pet('dog', 'willie')
+
+
+def describe_pet(animal_type, pet_name):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+    
+describe_pet('harry', 'hamster')
+
+
+def describe_pet(animal_type, pet_name):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+    
+describe_pet(animal_type='hamster', pet_name='harry')
+
+
+def describe_pet(pet_name, animal_type='dog'):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+    
+describe_pet(pet_name='willie')
+
+
+# Пес по имени Вилли.
+describe_pet('willie')
+describe_pet(pet_name='willie')
+
+
+# Хомяк по имени Гарри. 
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
