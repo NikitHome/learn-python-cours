75af01ebfe28b1e699d31e5639cf027fffcb5e69
Nikita Khomenko <mishanikita261202@mail.ru>
2025-01-24 13:51:13 +0300

    Update greeter.py

--- Themes/Theme_8/greeter.py
+++ Themes/Theme_8/greeter.py
@@ -1,4 +1,12 @@
+def greet_user():
+    """Выводит простое приветствие."""
+    print("Hello!")
+    
+greet_user()
+
+
 def greet_user(username):
+    """Выводит простое приветствие."""
     print(f"Hello, {username.title()}!")
     
-greet_user('john')
+greet_user('jesse')
