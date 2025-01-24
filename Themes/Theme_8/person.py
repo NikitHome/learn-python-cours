af2a13ffba147598889935862a13ba68eb015b06
Nikita Khomenko <mishanikita261202@mail.ru>
2025-01-24 13:51:15 +0300

    Update person.py

--- Themes/Theme_8/person.py
+++ Themes/Theme_8/person.py
@@ -1,12 +1,15 @@
+def build_person(first_name, last_name):
+    person = {'first': first_name, 'last': last_name}
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print(musician)
+
+
 def build_person(first_name, last_name, age=None):
-    person = {
-        'first': first_name,
-        'last': last_name,
-    }
-    
+    person = {'first': first_name, 'last': last_name}
     if age:
         person['age'] = age
-    
     return person
 
 musician = build_person('jimi', 'hendrix', age=27)
