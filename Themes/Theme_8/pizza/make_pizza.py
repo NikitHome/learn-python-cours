30a23cf1319e7fdd16cd544d5325e5139050198e
Nikita Khomenko <mishanikita261202@mail.ru>
2025-02-07 15:24:29 +0300

    Update make_pizza.py

--- Themes/Theme_8/pizza/make_pizza.py
+++ Themes/Theme_8/pizza/make_pizza.py
@@ -1,4 +1,28 @@
+import pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
+
+
 from pizza import make_pizza
+
+make_pizza(16, 'pepperoni')
+make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+from pizza import make_pizza as mp
+
+mp(16, 'pepperoni')
+mp(16, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+import pizza as p
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+from pizza import *
 
 make_pizza(16, 'pepperoni')
 make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
