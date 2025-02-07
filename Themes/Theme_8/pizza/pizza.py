b1052806ca42fab31c80e5f8aedc4f7baf578a58
Nikita Khomenko <mishanikita261202@mail.ru>
2025-02-07 15:24:30 +0300

    Update pizza.py

--- Themes/Theme_8/pizza/pizza.py
+++ Themes/Theme_8/pizza/pizza.py
@@ -1,4 +1,23 @@
+def make_pizza(*toppings):
+    print(toppings)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+def make_pizza(*toppings):
+    print(f"\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+        
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
 def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inch pizza with the following toppings:")
     for topping in toppings:
         print(f"- {topping}")
+        
+make_pizza(16, 'pepperoni')
+make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
