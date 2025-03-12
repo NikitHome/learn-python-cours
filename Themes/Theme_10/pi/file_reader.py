0ac0788f8903ccd723dd9fdfcfd78d6519c66339
Nikita Khomenko <mishanikita261202@mail.ru>
2025-03-12 16:10:05 +0300

    Update file_reader.py

--- Themes/Theme_10/pi/file_reader.py
+++ Themes/Theme_10/pi/file_reader.py
@@ -1,13 +1,17 @@
-with open('Themes/Theme_12/files/pi_digits.txt') as file_object:
+file_path = 'themes/theme_10/pi/pi_digits.txt'
+with open(file_path) as file_object:
     contents = file_object.read()
-    contents = contents.strip()
-print(contents)
+    print(contents.rstrip())
 
-with open('Themes/Theme_12/files/pi_digits.txt') as file_object:
+
+file_path = 'themes/theme_10/pi/pi_digits.txt'
+with open(file_path) as file_object:
     for line in file_object:
-        print(line)
+        print(line.rstrip())
 
-with open('Themes/Theme_12/files/pi_digits.txt') as file_object:
+file_path = 'themes/theme_10/pi/pi_digits.txt'
+
+with open(file_path) as file_object:
     lines = file_object.readlines()
     
 for line in lines:
