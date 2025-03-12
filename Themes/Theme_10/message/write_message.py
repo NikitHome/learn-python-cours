d24b890f2d6376d932678416502be1554ecf5652
Nikita Khomenko <mishanikita261202@mail.ru>
2025-03-12 16:10:03 +0300

    Update write_message.py

--- Themes/Theme_10/message/write_message.py
+++ Themes/Theme_10/message/write_message.py
@@ -1,5 +1,12 @@
-filename = 'Themes/Theme_12/files/programming.txt'
+filename = 'themes/theme_10/message/programming.txt'
 
 with open(filename, 'w') as file_object:
     file_object.write("I love programming!\n")
     file_object.write("I love creating new games.\n")
+    
+
+filename = 'themes/theme_10/message/programming.txt'
+
+with open(filename, 'a') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
