a36a0fb78962195b32bfc597165d3ba62c55053d
Nikita Khomenko <mishanikita261202@mail.ru>
2025-03-17 15:15:04 +0300

    Create greet_user.py

---
+++ themes/theme_10/user/greet_user.py
@@ -0,0 +1,13 @@
+import json
+
+filename = 'themes/theme_10/user/username.json'
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"We'll remember you when you come back, {username}!")
+else:
+    print(f"Welcome back, {username}!")
